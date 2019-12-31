15d406b0ac463ecfa86db77cc4d19179c4ab965e
firmanrizaldi <firmanrizaldiyusup@gmail.com>
2019-12-31 15:17:09 +0700

    udah beres sup

--- __manifest__.py
+++ __manifest__.py
@@ -19,8 +19,8 @@
 		"view/stock_opname.xml",
 		"view/workcenter.xml",
 		"sequence/squence.xml",
-		"data/mrp.workcenter.csv",
 		"data/storage.proses.csv",
+		"data/mrp.workcenter.csv",
   
 		# "report/stock_opname.xml",
 	],
